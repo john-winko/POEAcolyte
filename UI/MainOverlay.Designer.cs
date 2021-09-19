35943ea731503a193c94ef5c788136395f5e6f6b
Work-John <john.winko@gmail.com>
2021-09-19 11:34:24 -0500

    Main Overlay completed. working on adding basic trade interfaces next

--- UI/MainOverlay.Designer.cs
+++ UI/MainOverlay.Designer.cs
@@ -1,13 +1,12 @@
-﻿using System.ComponentModel;
-
+﻿
 namespace PoeAcolyte.UI
 {
-    partial class FullOverlay
+    partial class MainOverlay
     {
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private IContainer components = null;
+        private System.ComponentModel.IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -19,7 +18,6 @@ namespace PoeAcolyte.UI
             {
                 components.Dispose();
             }
-
             base.Dispose(disposing);
         }
 
@@ -31,31 +29,38 @@ namespace PoeAcolyte.UI
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
-            // Overlay
+            // btnExit
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.btnExit.Location = new System.Drawing.Point(81, 123);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // MainOverlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            // this.Controls.Add(this.lblInfo);
-            // this.Controls.Add(this.tradesPanel);
-            // this.Controls.Add(this.btnTest);
-            // this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnExit);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Overlay";
-            this.Text = "Overlay";
+            this.Name = "MainOverlay";
+            this.Text = "MainOverlay";
             this.TopMost = true;
             this.TransparencyKey = System.Drawing.Color.Lime;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            //this.contextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
-            // this.components = new System.ComponentModel.Container();
-            // this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            // this.ClientSize = new System.Drawing.Size(800, 450);
-            // this.Text = "FullOverlay";
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnExit;
     }
 }
